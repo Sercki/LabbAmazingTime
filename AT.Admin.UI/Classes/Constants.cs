7ab74bae7c29c8dd110d6c5359251707bc868456
Sergiusz Radecki <112434642+Sercki@users.noreply.github.com>
2023-02-16 22:06:43 +0100

    added prototype of admin UI

---
+++ AT.Admin.UI/Classes/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace AT.Admin.UI.Classes;
+
+public static class Routes
+{
+	public static string Directors => "Directors";
+	public static string Films => "Films";
+	public static string Genres => "Genres";
+	public static string FilmGenres => "FilmGenres";
+	public static string SimilarFilms => "SimilarFilms";
+	public static string Register => "Register";
+}
+
+public static class PageType
+{
+	public static string Index => "Index";
+	public static string Create => "Create";
+	public static string Edit => "Edit";
+	public static string Delete => "Delete";
+}
--- AT.Admin.UI/Usings.cs
+++ AT.Admin.UI/Usings.cs
@@ -1,1 +1,1 @@
-﻿
+﻿global using AT.Admin.UI.Classes;
